686ca4706099cb577dd0c1fb9565b05e4b746f29
Bignon7 <hindemelais@gmail.com>
2026-03-02 19:15:50 +0100

    How streamlit works

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
-st.set_page_config(page_title="To-do list", page_icon=":memo:")
+st.set_page_config(page_title="To-do list", page_icon=":house:")
 
 st.markdown(":blue[Texte en bleu] et :red[texte en rouge]")
 st.markdown(":green-background[Texte avec fond vert]")
 
 st.title(
-        ":orange[:material/checklist:] To-do list",
+        ":orange[:material/home:] My HOME PAGE",
         width="content",
         anchor=False,
     )
@@ -18,3 +18,16 @@ st.toggle("Toggle me")
 st.checkbox("Check me")
 
 st.toast("This is a toast message")
+
+locations = (
+    "New York",
+    "Los Angeles",
+    "Chicago",
+    "Houston",
+    "Phoenix",
+)
+
+st.selectbox("Select a location", options=locations)
+
+st.text_input("Enter your name")
+st.text_area("Enter your address")
